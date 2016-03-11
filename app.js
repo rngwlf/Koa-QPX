16091f49ab960947c81369e4008f2dd9b734bb69
rng_wlf <rng.wlf@gmail.com>
2016-03-10 23:08:40 -0700

    Init

---
+++ app.js
@@ -0,0 +1,119 @@
+var koa = require('koa')
+	, serve = require('koa-static')
+	, route = require('koa-route')
+	, request = require('co-request')
+	, moment = require('moment')
+	, currencies = require('country-data').currencies
+	, fs = require('fs');
+
+var app = koa();
+
+var jsonBuilder = {};
+var serviceConfig = JSON.parse(fs.readFileSync('service_config.json','utf8'));
+//Object containing potential JSON builders for 3rd party APIs
+jsonBuilder['JSONPath-Template'] = require('./json-builder');
+
+// DELETE
+app.use(serve('sampledata/'));
+
+app.use(route.get('/flights',flights));
+
+//Randomly choose service from service_config to spreadout API calls
+function getService(){
+	function getRandomInt(min, max) {
+	  return Math.floor(Math.random() * (max - min)) + min;
+	}
+	var servicesCount = serviceConfig.services.length;
+	return serviceConfig.services[getRandomInt(0,servicesCount)];
+}
+
+function *flights(){
+	if(validParameters(this.query)){
+		var service = getService();
+		var searchParameters = jsonBuilder[service.type].getServiceRequest(this.query, service);
+		console.log(searchParameters);
+		this.body = searchParameters;
+		/*
+		var serviceResponse = yield request({
+						uri: service.url
+						, body : searchParameters
+						, json: true});
+		console.log(serviceResponse);
+		this.body=serviceResponse;
+		var results = JSON.parse(serviceResponse.body);
+		console.log(results);
+		if(results.error == undefined){
+			this.body = jsonBuilder[service.type].getResponse(results, service);
+		}
+		else{
+			this.body = results;
+		}
+		*/
+		
+	}
+	else{
+		this.body = {error: "input invalid"};
+	}
+	
+}
+
+
+
+function validParameters(parameters){
+	var isValid = true;
+
+	if(parameters.start_date == undefined ||
+		parameters.adult_passengers == undefined ||
+		parameters.origin == undefined ||
+		parameters.destination == undefined ||
+		parameters.max_stops == undefined ||
+		parameters.max_price == undefined ||
+		parameters.max_price_currency == undefined){
+
+		isValid = false;
+		console.log("missing parameter");
+	}
+	else{
+		if(isNaN(parameters.adult_passengers)|| parseInt(parameters.adult_passengers) <= 0){
+			isValid = false;
+		}
+		else if(isNaN(parameters.max_stops) || parseInt(parameters.max_stops) < 0){
+			isValid = false;
+		}
+		else if(typeof parameters.origin != "string" || typeof parameters.destination != "string"){
+			isValid = false;
+		}
+		else if(typeof parameters.max_price_currency != "string" || currencies[parameters.max_price_currency] == undefined){
+			isValid = false;
+		}
+		else if(isNaN(parameters.max_price) || parseInt(parameters.max_price) <= 0){
+			isValid = false;
+		}
+		else{
+			parameters.adult_passengers = parseInt(parameters.adult_passengers);
+			parameters.max_stops = parseInt(parameters.max_stops);
+			parameters.max_price = parseInt(parameters.max_price);
+		}
+
+
+	}
+
+
+	if(parameters.start_date != undefined && 
+	   !moment(parameters.start_date, "YYYY-MM-DD HH:mm:ss",true).isValid()){
+		isValid = false;
+		console.log(parameters.start_date);
+		console.log("start date error");
+	}
+
+	if(parameters.return_date != undefined && 
+	   !moment(parameters.return_date, "YYYY-MM-DD HH:mm:ss", true).isValid()){
+		isValid = false;
+		console.log("return date error");
+	}
+
+	return isValid;
+}
+
+
+app.listen(3000);
---
+++ json-builder.js
@@ -0,0 +1,128 @@
+var JSONPath = require('jsonpath-plus');
+var moment = require('moment');
+
+var public = {};
+
+function getParameter(path, object){
+	//console.log(path);
+	return JSONPath({wrap: false}, path, object);
+}
+function getParameters(path, object){
+	//console.log(path);
+	return JSONPath({wrap: true}, path, object);
+}
+
+
+public.getResponse = function (results, service){
+	var response = {results: []};
+	var mapping = service.response_jsonmapping;
+	var options = getParameters(mapping.base, results);
+
+	//Loop through result set
+	options.forEach(function(option){
+		var trips = [];
+		response.results.push({
+			total_price : getParameter(mapping.total_price, option)
+			, departure_time : getParameter(mapping.departure_time, option)
+			//Return arrival time will not be returned if one way
+			, return_arrival_time : getParameter(mapping.return_arrival_time, option) 
+			, trips : trips
+		});
+
+		//Get trips, 2 trips for a round trip request, 1 otherwise
+		var tripsMapping = mapping.trips;
+		var optionTrips = getParameters(tripsMapping.base, option);
+		optionTrips.forEach(function(trip){
+
+			var flights = [];
+
+			trips.push({
+				total_duration : getParameter(tripsMapping.total_duration, trip)
+				, departure_time : getParameter(tripsMapping.departure_time, trip)
+				, arrival_time : getParameter(tripsMapping.arrival_time, trip)
+				, flights : flights
+			});
+
+			//Get flights within the trip
+			var flightMapping = mapping.trips.flights;
+			flightOptions = getParameters(flightMapping.base, trip);
+			flightOptions.forEach(function(flight){
+				flights.push({
+					duration : getParameter(flightMapping.duration, flight)
+					, carrier : getParameter(flightMapping.carrier, flight)
+					, flight_number : getParameter(flightMapping.flight_number, flight)
+					, connection_time : getParameter(flightMapping.connection_time, flight)
+					, departure_time : getParameter(flightMapping.departure_time, flight)
+					, arrival_time : getParameter(flightMapping.arrival_time, flight)
+					, origin_airport : getParameter(flightMapping.origin_airport, flight)
+					, destination_airport : getParameter(flightMapping.destination_airport, flight)
+				})
+			});
+		});
+	});
+
+	return response;
+}
+
+
+public.getServiceRequest = function (request, service)
+{
+	var mapping = service.request_jsonmapping;
+	//Safely clone mapping
+	var serviceRequest = JSON.parse(JSON.stringify(service.request_jsonmapping));
+
+	buildObject(serviceRequest);
+
+	function buildObject(inputObj){
+		var properties = Object.keys(inputObj);
+
+
+		properties.forEach(function(property){
+			
+			if(typeof inputObj[property] == "string" && inputObj[property].indexOf("$.") > -1){
+				var mapPath = inputObj[property];
+				var propertyValue = null;
+
+				if(mapPath.indexOf("-INT") > -1){
+					mapPath = mapPath.split('-')[0];
+					propertyValue = parseInt(getParameter(mapPath, request));
+				}
+				else if(mapPath.indexOf("+") > -1){
+					var mapPaths = mapPath.split('+');
+					propertyValue = "";
+					mapPaths.forEach(function(path){
+						propertyValue += getParameter(path, request);
+					});
+				}
+				else if(mapPath.indexOf("-DATE:") > -1){
+					var mapPaths = mapPath.split("-DATE:");
+					console.log(mapPath);
+					console.log(mapPaths[0]);
+					console.log(mapPaths[1]);
+					console.log(getParameter(mapPaths[0], request));
+					propertyValue = moment(getParameter(mapPaths[0], request),"YYYY-MM-DD HH:mm:ss").format(mapPaths[1]);
+					console.log(propertyValue);
+				}
+				else{
+					propertyValue = getParameter(mapPath, request)
+				}
+
+
+				inputObj[property] = propertyValue;
+				
+			}
+			else if(typeof inputObj[property] == "string" && inputObj[property] == "#api_key"){
+				inputObj[property] = service.api_key;
+			}
+			else if(typeof inputObj[property] == "object"){
+				var subObject = inputObj[property];
+				if(subObject._OPTIONALOBJECT != undefined && getParameter(subObject._OPTIONALOBJECT,request) != undefined){
+					buildObject(inputObj[property]);
+				}
+			}
+		});
+	}
+	return serviceRequest;
+}
+
+module.exports = public;
